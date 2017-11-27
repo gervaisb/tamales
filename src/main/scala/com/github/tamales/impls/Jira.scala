351e9c76a4a47059304ffab45f592a342b82ab9e
gervaisb <gervais.b@gmail.com>
2017-11-27 21:19:09 +0100

    Terminate itself when done

--- src/main/scala/com/github/tamales/impls/Jira.scala
+++ src/main/scala/com/github/tamales/impls/Jira.scala
@@ -2,16 +2,13 @@ package com.github.tamales.impls
 
 import java.io.IOException
 import java.net.{URI, URL, URLEncoder}
-import java.security.SecureRandom
-import java.security.cert.X509Certificate
 import java.util.Base64
-import javax.net.ssl._
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, PoisonPill, Props}
 import com.github.tamales.Provider.Refresh
 import com.github.tamales.Publisher.TaskFound
 import com.github.tamales.{ActorConfig, Task, TaskId, TasksEventBus}
-import okhttp3.{OkHttpClient, Request, Response}
+import okhttp3.{Request, Response}
 import play.api.libs.json.{JsArray, JsObject, Json}
 
 import scala.collection.mutable
@@ -24,13 +21,14 @@ class Jira(val events: TasksEventBus) extends Actor with ActorLogging with Actor
   private val cfg = new Cfg
   private val http = UnsafeOkHttpClientBuilder.build()
 
-  override def receive = {
+  override def receive:Receive = {
     case Refresh =>
       log.info("Refreshing tasks")
       search().foreach { task =>
         log.info(s"Found ${task.id} from Jira")
         events.publish(TaskFound(task, self))
       }
+      self ! PoisonPill // No need to get event back
   }
 
   private def search():Seq[Task] = {
@@ -62,7 +60,7 @@ class Jira(val events: TasksEventBus) extends Actor with ActorLogging with Actor
     if ( fields!=null ) {
       params += "fields="+URLEncoder.encode(fields, "UTF-8")
     }
-    if ( !params.isEmpty) {
+    if ( params.nonEmpty ) {
       path + params.mkString("?", "&", "")
     } else {
       path
@@ -85,10 +83,10 @@ class Jira(val events: TasksEventBus) extends Actor with ActorLogging with Actor
   }
 
   class Cfg {
-    val location = config.getString("providers.jira.location")
-    val host = new URL(location).getHost
-    val username = config.getString("providers.jira.username")
-    val password = config.getString("providers.jira.password")
+    val location:String = config.getString("providers.jira.location")
+    val host:String = new URL(location).getHost
+    val username:String = config.getString("providers.jira.username")
+    val password:String = config.getString("providers.jira.password")
   }
 
 }
